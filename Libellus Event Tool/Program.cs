56ffda29131cfca725f2b32ac282197b9a0884f8
darkitxu84 <76633266+darkitxu84@users.noreply.github.com>
2026-06-03 20:53:55 -0400

    cli improvements
    * new helper function Warn to print warning messages
    * print the usage when the user provides no arguments
    * handle exceptions when reading/creating a pm1 file to provide descriptive errors
    * skip to the next path if the current path is not a file nor directory
    * minors spelling mistakes

--- Libellus Event Tool/Program.cs
+++ Libellus Event Tool/Program.cs
@@ -8,20 +8,31 @@ namespace LibellusEventTool
 		/// Controls whether to convert all PMD or JSON files contained within a passed folder and it's subfolders.
 		/// </summary>
 		private static bool _recurse = false;
+
+		private static void Warn(string message)
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.Write($"[WARN] ");
+			Console.ResetColor();
+			Console.WriteLine(message);
+		}
 		static async Task Main(string[] args)
 		{
 			System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
 			System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
 			string version = fvi.FileVersion ?? "Null Version"; // Address CS8600 warning
+			string fileName = Path.GetFileNameWithoutExtension(assembly.Location);
 			Console.WriteLine($"Welcome to LEET!\nLibellus Event Editing Tool: v{version}\nNow with better syntax!\n");
 
 			_recurse = args.Contains("-r", StringComparer.OrdinalIgnoreCase);
 			int numberPaths = _recurse ? args.Length - 1 : args.Length;
 			if (numberPaths < 1)
 			{
-				Console.ForegroundColor = ConsoleColor.Yellow;
-				Console.WriteLine("Not enough args!");
-				Console.ResetColor();
+				Warn("Not enough args!\n");
+				Console.WriteLine($"usage: \"{fileName}\" [-r] <PATH>...");
+				Console.WriteLine("options:");
+				Console.WriteLine("  -r,    Controls whether to convert all PMD or JSON\n" + 
+				                  "         files contained within a passed folder and it's subfolders.\n");
 				Console.WriteLine("Press any button to exit.");
 				Console.ReadKey();
 				return;
@@ -33,29 +44,53 @@ namespace LibellusEventTool
 
 		private static async Task ConvertPaths(string[] paths)
 		{
-			foreach (string file in paths)
+			foreach (string path in paths)
 			{
-				string ext = Path.GetExtension(file).ToLower();
+				if (!File.Exists(path) && !Directory.Exists(path))
+				{
+					Warn($"'{path}' is not a valid file or directory!");
+					continue;
+				}
+				
+				string ext = Path.GetExtension(path).ToLower();
 				if (ext == ".pm1" || ext == ".pm2" || ext == ".pm3")
 				{
-					Console.WriteLine($"Coverting to Json: {file}");
+					Console.WriteLine($"Converting to Json: {path}");
 					PmdReader reader = new();
-					PolyMovieData pmd = await reader.ReadPmd(file);
+					PolyMovieData pmd = await reader.ReadPmd(path);
 					// the "!" in Path.GetDirectoryName(file)! indicates null forgiveness; should be safe & addresses CS8604
-					string folder = Path.Combine(Path.GetDirectoryName(file)!, Path.GetFileNameWithoutExtension(file));
-					await pmd.ExtractPmd(folder, Path.GetFileName(file));
+					string folder = Path.Combine(Path.GetDirectoryName(path)!, Path.GetFileNameWithoutExtension(path));
+					try
+					{
+						await pmd.ExtractPmd(folder, Path.GetFileName(path));
+					}
+					catch (Exception ex)
+					{
+						Warn($"Cannot extract '{path}': {ex.Message}");
+					}
 				}
 				else if (ext == ".json")
 				{
-					Console.WriteLine($"Coverting to PMD: {file}");
-					PolyMovieData pmd = await PolyMovieData.LoadPmd(file);
-					pmd.SavePmd($"{file}.PM{pmd.MagicCode[3]}");
+					Console.WriteLine($"Converting to PMD: {path}");
+					PolyMovieData pmd = new PolyMovieData();
+					try
+					{
+						pmd = await PolyMovieData.LoadPmd(path);
+					}
+					catch (Exception ex)
+					{
+						Warn($"Cannot convert '{path}' to PMD: {ex.Message}");
+						continue;
+					}
+					
+					pmd.SavePmd($"{path}.PM{pmd.MagicCode[3]}");
 				}
-				else if (Directory.Exists(file))
+				else if (Directory.Exists(path))
 				{
-					await ConvertPaths(Directory.GetFiles(file, "*", _recurse ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly));
+					await ConvertPaths(Directory.GetFiles(path, "*", _recurse ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly));
 				}
 			}
+			Console.Write("\n");
 		}
 	}
 }
